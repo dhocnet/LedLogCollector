56bd0fff4566630591e7f2171a07deeea0c3066f
DHOCNETwork <dhocnet@gmail.com>
2018-09-21 15:20:40 +0800

    Update ledlogcollector.py

--- ledlogcollector.py
+++ ledlogcollector.py
@@ -492,6 +492,7 @@ def llc_kerja(llc_ar):
     if not os.path.exsists("cache"):
         os.mkdir("cache")
     os.chdir("cache")
+    shutil.rmtree(".")
     llc_fdir = os.listdir(llc_ar)
     print "Ditemukan: %d direktori log..."%len(llc_fdir)
     for llc_pil in llc_fdir:
@@ -567,7 +568,6 @@ def llc_bantuan():
 if not os.path.exists(llc_out):
     os.mkdir(llc_out)
 os.chdir(llc_out)
-shutil.rmtree("cache\\.")
 
 if os.path.exists(llc_32):
     llc_kerja(llc_32)
